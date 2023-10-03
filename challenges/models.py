cce0782767dbc4b45750e19913a5255799703f7c
eliseius <eliseevsi@gmail.com>
2023-10-04 00:07:13 +0300

    Level 2 task A

--- challenges/models.py
+++ challenges/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import json
+
 
 class Book(models.Model):
     title = models.CharField(max_length=256)
@@ -8,3 +10,31 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Laptop(models.Model):
+    class Meta:
+        db_table = 'Laptop_info'
+        get_latest_by = 'created_at'
+        ordering = ['-created_at']
+
+    brand = models.CharField(max_length=120)
+    year_of_issue = models.DateField()
+    amount_of_ram = models.PositiveSmallIntegerField()
+    hd_capacity = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+    quantity_in_stock = models.PositiveSmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def to_json(self):
+        data = {
+            'brand': self.brand,
+            'year_of_issue': self.year_of_issue.strftime('%Y'),
+            'amount_of_ram': self.amount_of_ram,
+            'hd_capacity': self.hd_capacity,
+            'price': self.price,
+            'quantity_in_stock': self.quantity_in_stock,
+            'created_at':self.created_at.strftime('%d-%m-%Y %H:%M:%S')
+        }
+
+        return json.dumps(data)
--- challenges/views/level_2/a_laptops.py
+++ challenges/views/level_2/a_laptops.py
@@ -13,13 +13,18 @@
 """
 from django.http import HttpRequest, HttpResponse
 
+from challenges.models import Laptop
+
 
 def laptop_details_view(request: HttpRequest, laptop_id: int) -> HttpResponse:
     """
     В этой вьюхе вам нужно вернуть json-описание ноутбука по его id.
     Если такого id нет, вернуть 404.
     """
-    pass
+    laptop = Laptop.objects.filter(id=laptop_id).first()
+    if laptop is None:
+        return HttpResponse(status=404)
+    return HttpResponse(laptop.to_json())
 
 
 def laptop_in_stock_list_view(request: HttpRequest) -> HttpResponse:
@@ -27,7 +32,8 @@ def laptop_in_stock_list_view(request: HttpRequest) -> HttpResponse:
     В этой вьюхе вам нужно вернуть json-описание всех ноутбуков, которых на складе больше нуля.
     Отсортируйте ноутбуки по дате добавления, сначала самый новый.
     """
-    pass
+    all_laptops = Laptop.objects.filter(quantity_in_stock__gte=0).order_by('created_at')
+    return HttpResponse([product.to_json() for product in all_laptops])
 
 
 def laptop_filter_view(request: HttpRequest) -> HttpResponse:
@@ -37,12 +43,29 @@ def laptop_filter_view(request: HttpRequest) -> HttpResponse:
     Если бренд не входит в список доступных у вас на сайте или если цена отрицательная, верните 403.
     Отсортируйте ноутбуки по цене, сначала самый дешевый.
     """
-    pass
+    data = request.GET
+    brand = data['brand']
+    min_price = data['min_price']
+    
+    laptops = Laptop.objects.all()
+    all_brands = set()
+    for laptop in laptops:
+        all_brands.add(laptop.brand)
+    
+    if brand in all_brands and int(min_price) > 0:
+        laptops = Laptop.objects.filter(price__gte=min_price, brand=brand).order_by('price')
+        return HttpResponse([laptop.to_json() for laptop in laptops])
+    else:
+        return HttpResponse(status=403)
+    
 
-
-def last_laptop_details_view(request: HttpRequest, laptop_id: int) -> HttpResponse:
+def last_laptop_details_view(request: HttpRequest) -> HttpResponse:
     """
     В этой вьюхе вам нужно вернуть json-описание последнего созданного ноутбука.
     Если ноутбуков нет вообще, вернуть 404.
     """
-    pass
+    last_laptop = Laptop.objects.order_by('-created_at').first()
+
+    if last_laptop:
+        return HttpResponse(last_laptop.to_json())
+    return HttpResponse(status=404)
